a821caf269f898aaea541166d7fc7603681218e2
Michael Rothgang <rothgami@math.hu-berlin.de>
2023-09-04 22:27:11 +0200

    Style improvements: put spaces around colons; extract some variables.

--- Sard/Stuff.lean
+++ Sard/Stuff.lean
@@ -22,6 +22,8 @@ variable
   [SmoothManifoldWithCorners J N] [FiniteDimensional ℝ F]
   [MeasurableSpace F] (μ: Measure F) [IsAddHaarMeasure μ]
 
+variable {m n r : ℕ} (hm : finrank ℝ E = m) (hn : finrank ℝ F = n) (hr : r > m-n)
+
 -- definition: measure zero subset of N
 -- for all charts (φ, U) of N, φ(U∩ S) ⊂ R^n has measure zero 
 def measure_zero (s: Set N) : Prop :=
@@ -32,15 +34,13 @@ def measure_zero (s: Set N) : Prop :=
 -- xxx this is the wrong statement: need "measure zero" concept on manifolds!
 -- this part is simpler in finite dimensions!!
 
-/-
- **Sard's theorem**. Let $M$ and $N$ be real $C^r$ manifolds of dimensions $m$ and $n$,
-and $f:M→N$ a $C^r$ map. If $r>\max{0, m-n}$, the set of regular values of $f$ has full measure.
--/
-theorem sard (m n r: ℕ) (hm: finrank ℝ E = m) (hn: finrank ℝ F = n) (hr: r > m-n)
-  (f:M→ N) (hf: ContMDiff I J r f)
+/- **Sard's theorem**. Let $M$ and $N$ be real $C^r$ manifolds of dimensions
+$m$ and $n$, and $f:M→N$ a $C^r$ map. If $r>\max{0, m-n}$,
+the set of regular values of $f$ has full measure. -/
+theorem sard (f : M → N) (hf : ContMDiff I J r f)
   (f' : ∀x, TangentSpace I x →L[ℝ] TangentSpace J (f x))
-  (s: Set M) (hf' : ∀ x ∈ s, HasMFDerivWithinAt I J f s x (f' x))
-  (h'f' : ∀ x ∈ s, ¬ Surjective (f' x)) : measure_zero J μ (f '' s) := by
+  (s : Set M) (hf' : ∀ x ∈ s, HasMFDerivWithinAt I J f s x (f' x))
+  (h'f' : ∀ x ∈ s, ¬ Surjective (f' x)) : measure_zero J (f '' s) := by
     sorry
 
 -- Corollary. The set of regular values is residual and therefore dense.
