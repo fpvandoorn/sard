34af2de29d6039facb134d4aaafa72e6a10a86e7
Michael Rothgang <rothgami@math.hu-berlin.de>
2023-09-09 20:26:37 +0200

    Further progress: statements complete, working on first proof.

--- Sard/Stuff.lean
+++ Sard/Stuff.lean
@@ -23,25 +23,70 @@ variable {m n r : ℕ} (hm : finrank ℝ E = m) (hn : finrank ℝ F = n) (hr : r
 variable {J}
 
 section ImageMeasureZeroSet
+/-- If $$f : X → Y$$ is a Lipschitz map between metric spaces, then `f` maps null sets
+to null sets, w.r.t. the `d`-dimensional Hausdorff measure (for $$d ∈ ℕ$$) on `X` resp. `Y`. -/
+-- this is just an auxiliary lemma -> perhaps inline later
+lemma lipschitz_image_null_set_is_null_set
+    { X Y : Type } [MetricSpace X] [MeasurableSpace X] [BorelSpace X]
+    [MetricSpace Y] [MeasurableSpace Y] [BorelSpace Y]
+    { f : X → Y } (hf : ∃ K : NNReal, LipschitzWith K f)
+    { d : ℝ } (hd : d ≥ 0) { s : Set X } (hs : μH[d] s = 0) : μH[d] (f '' s) = 0 := by
+  obtain ⟨K, hk⟩ := hf
+  have aux : μH[d] (f '' s) ≤ (K : ENNReal) ^ d * μH[d] s :=
+    LipschitzOnWith.hausdorffMeasure_image_le (LipschitzWith.lipschitzOnWith hk s) hd
+  rw [hs] at aux
+  simp_all only [ge_iff_le, mul_zero, nonpos_iff_eq_zero, le_refl, hs]
+
 /-- Consider metric spaces `X` and `Y` with the `d`-dimensional Hausdorff measure.
 If `X` is $σ$-compact, a locally Lipschitz map $f : X → Y$
 maps null sets in `X` to null sets in `Y`. -/
 lemma locally_lipschitz_image_of_null_set_is_null_set { X Y : Type }
     [MetricSpace X] [MeasurableSpace X] [BorelSpace X] [SigmaCompactSpace X]
     [MetricSpace Y] [MeasurableSpace Y] [BorelSpace Y] { d : ℕ }
-    { f : X → Y } --(hf : LocallyLipschitzWith 1 f)
+    { f : X → Y } (hf : ∀ x : X, ∃ K : NNReal, ∃ U : Set X, IsOpen U ∧ LipschitzOnWith K f U)
     { s : Set X } (hs : μH[d] s = 0) : μH[d] (f '' s) = 0 :=
-  -- argue with open cover and use `lipschitz_image_null_set_is_null_set`
+  -- Choose a cover of X by compact sets K_i.
+  let cov : ℕ → Set X := sorry -- SigmaCompactSpace.compact_covering X
+  have hcov : ⋃ (n : ℕ), cov n = univ := by sorry -- iUnion_compactCovering
+  have hcompact : ∀ n : ℕ, IsCompact (cov n) := by sorry -- isCompact_compactCovering
+  -- It suffices to show the statement for all K_i. Let i be arbitrary.
+  suffices ass : ∀ n : ℕ, μH[d] (f '' (s ∩ cov n)) = 0 by
+    have : s = ⋃ (n : ℕ), s ∩ cov n := by
+      -- FIXME: I'm sure this can be golfed into oblivion!
+      ext x
+      constructor
+      · intro hx
+        rw [mem_iUnion]
+        have h : x ∈ s ∩ ⋃ (n : ℕ), cov n := by
+          rw [← inter_univ s] at hx
+          rw [← hcov] at hx
+          exact hx
+        have : x ∈ ⋃ (n : ℕ), cov n := mem_of_mem_inter_right h
+        have sdf : ∃ i, x ∈ cov i := Iff.mp mem_iUnion this
+        rcases sdf with ⟨i, hi⟩
+        use i
+        exact mem_inter hx hi
+      · intro hx
+        rw [mem_iUnion] at hx
+        obtain ⟨i, hi⟩ := hx
+        exact mem_of_mem_inter_left hi
+    have hless : μH[d] (f '' s) ≤ 0 := by
+      calc μH[d] (f '' s)
+        _ = μH[d] (f '' (⋃ (n : ℕ), s ∩ cov n)) := by rw [← this]
+        _ = μH[d] (⋃ (n : ℕ), f '' (s ∩ cov n)) := by rw [@image_iUnion]
+        _ ≤ ∑' (n : ℕ), μH[d] (f '' (s ∩ cov n)) := by apply OuterMeasure.iUnion_nat
+        _ = ∑' (n : ℕ), 0 := by simp_rw [ass]
+        _ = 0 := by rw [@tsum_zero]
+    have hmore : 0 ≤ μH[d] (f '' s) := by simp only [ge_iff_le, zero_le]
+    -- XXX. is this even true? or does this only hold for NNReals?
+    --have aux : ∀ a b : ENNReal, a ≤ b ∧ b ≤ a → a = b := by sorry
+    --have aux2 : ∀ a b : NNReal, a ≤ b ∧ b ≤ a → (a : ℝ) = (b : ℝ) := by rw?
+    sorry
+  --intro n
+  -- Consider the cover induced by hf. This has a finite subcover as K_i is compact.
+  -- On each open, f is Lipschitz by hypothesis.
+  -- Hence, the previous lemma `lipschitz_image_null_set_is_null_set` applies.
   sorry
-
-/-- If $$f : X → Y$$ is a Lipschitz map between metric spaces, then `f` maps null sets
-to null sets, w.r.t. the `d`-dimensional Hausdorff measure (for $$d ∈ ℕ$$) on `X` resp. `Y`. -/
-lemma lipschitz_image_null_set_is_null_set
-    { X Y : Type } [MetricSpace X] [MeasurableSpace X] [BorelSpace X]
-    [MetricSpace Y] [MeasurableSpace Y] [BorelSpace Y] { f : X → Y } (hf : ∃ K : NNReal, LipschitzWith K f)
-    { d : ℕ } { s : Set X } (hs : μH[d] s = 0) : μH[d] (f '' s) = 0 :=
-  sorry -- mostly in mathlib: Lipschitz maps and Hausdorff measure are related
-  -- mostly just a Lemma
 
 /-- Let $U c ℝ^n$ be an open set and f : U → ℝ^n be a C^1 map.
   If $X\subset U$ has measure zero, so has $f(X)$.
@@ -57,7 +102,6 @@ lemma C1_image_null_set_null {f : E → F} {U : Set E} (hU : IsOpen U) (hf : Con
   -- Lebesgue measure is the Haar measure on R^n -> should follow
   sorry
 end ImageMeasureZeroSet
-#exit
 
 -- An open subset of a topological manifold contains an interior point (not on the boundary). -/
 -- lemma open_subset_contains_interior_point : (s : Set N) (hs : IsOpen s) :
