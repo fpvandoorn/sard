5e470244f4ca93b19cbcd649d613d3b28ad2524d
Michael Rothgang <rothgami@math.hu-berlin.de>
2023-09-22 21:29:46 +0200

    wip: try generalising to an open subset. Getting stuck at coercion errors; need to make real extra assumptions.

--- Sard/LocallyLipschitzMeasureZero.lean
+++ Sard/LocallyLipschitzMeasureZero.lean
@@ -32,6 +32,58 @@ lemma lipschitz_image_null_set_is_null_set {d : ℝ} (hd : d ≥ 0) {s : Set X} 
   simp only [mul_zero, nonpos_iff_eq_zero, hs] at aux ⊢
   exact aux
 
+-- version specialized to an open set. proof should be completely analogous
+lemma locally_lipschitz_image_of_null_set_is_null_set_open [SigmaCompactSpace X] {d : ℕ}
+    {f : X → Y} {W : Set X} (hW : IsClosed W) (hf : LocallyLipschitz (W.restrict f))
+    {s : Set X} (hsu : s ⊆ W) (hs : μH[d] s = 0) : μH[d] (f '' s) = 0 := by
+  -- Choose a countable cover of X by compact sets K_n.
+  let K : ℕ → Set X := compactCovering X
+  have hcov : ⋃ (n : ℕ), K n = univ := iUnion_compactCovering X
+  have hcompact : ∀ n : ℕ, IsCompact (K n) := isCompact_compactCovering X
+  -- By countable subadditivity, it suffices to show the claim for each K_n.
+  suffices ass : ∀ n : ℕ, μH[d] (f '' (s ∩ W ∩ K n)) = 0 by sorry
+    --have : f '' (s ∩ W ∩ K n) = f '' (s ∩ K n) := sorry
+    --rewrite [← this] --- fold into the above?
+    -- apply null_set_from_countable_cover _ _ ass
+    -- rw [← image_iUnion]
+    -- have : s = ⋃ (n : ℕ), s ∩ K n := by calc s
+    --     _ = s ∩ univ := (inter_univ s).symm
+    --     _ = s ∩ ⋃ (n : ℕ), K n := by rw [hcov]
+    --     _ = ⋃ (n : ℕ), s ∩ K n := by apply inter_iUnion s
+    -- exact Eq.subset (congrArg (image f) this)
+
+  intro n
+  -- Consider the cover (t_x) of W ∩ K_n induced by hf: for each x ∈ W,
+  -- choose a neighbourhood t_x of x and a constant K_x such that f is K_x-Lipschitz on t_x.
+  choose Kx t ht hfL using fun x ↦ (hf x)
+  -- For each t_x, choose an open set U_x ∋ x contained in t_x.
+  choose U hut hUopen hxU using fun x ↦ mem_nhds_iff.mp (ht x)
+
+  -- By construction, (U_x) is an open covering of W ∩ K_n.
+  --have hcovering : K n ⊆ ⋃ (x : X), U x := fun y _ ↦ mem_iUnion_of_mem y (hxU y)
+  have hcovering : W ∩ K n ⊆ ⋃ (x : W), U x := by
+    intro y hy
+    sorry --aesop-- let sdf := hxU y
+    -- apply [mem_iUnion_of_mem y]
+    --exact?
+    --fun y hy ↦ mem_iUnion_of_mem y (hxU y)
+  --#exit
+  -- As W is closed, W ∩ K_n is also compact. Hence, (U_x) has a finite subcover V_1, ..., V_l.
+  have : IsCompact (W ∩ K n) := (hcompact n).inter_left hW
+  have cov' : W ∩ K n ⊆ ⋃ (i : X) (_ : i ∈ ↑W), U i := sorry
+
+  let ⟨v, hv⟩ := IsCompact.elim_finite_subcover this U hUopen cov'
+  #exit
+
+  -- f is Lipschitz on each U_j, hence the previous lemma applies.
+  have hnull: ∀ i : v, μH[d] (f '' (s ∩ U i)) = 0 := by
+    intro i
+    have h₂ : μH[d] (s ∩ U i) = 0 := measure_mono_null (inter_subset_left s (U ↑i)) hs
+    have h₁ := ((hfL i).mono (hut i)).mono (inter_subset_right s (U i))
+    refine lipschitz_image_null_set_is_null_set (Nat.cast_nonneg d) h₁ h₂
+
+
+  #exit
 /-- Consider two metric spaces `X` and `Y` with the `d`-dimensional Hausdorff measure.
 If `X` is `σ`-compact, a locally Lipschitz map `f : X → Y` maps null sets in `X` to null sets in `Y`. -/
 lemma locally_lipschitz_image_of_null_set_is_null_set [SigmaCompactSpace X] {d : ℕ}
@@ -85,9 +137,4 @@ lemma locally_lipschitz_image_of_null_set_is_null_set [SigmaCompactSpace X] {d :
       _ = 0 := by simp
   simp only [nonpos_iff_eq_zero, zero_le] at hless ⊢
   exact hless
-
--- version specialized to an open set. proof should be completely analogous
-lemma locally_lipschitz_image_of_null_set_is_null_set_open [SigmaCompactSpace X] {d : ℕ}
-    {f : X → Y} {U : Set X} (hf : LocallyLipschitz (U.restrict f))
-    {s : Set X} (hsu : s ⊆ U) (hs : μH[d] s = 0) : μH[d] (f '' s) = 0 := by sorry
 end ImageMeasureZeroSet
